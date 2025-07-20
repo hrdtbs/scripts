c5c224d35efa794836c2942cb18f7222bb79cbcb
hrdtbs <harada.tsubasa.0422@gmail.com>
2025-07-20 23:51:40 +0900

    feat: TUIメニューの選択肢に実行関数を追加し、選択肢の処理を簡素化

--- tui/index.ts
+++ tui/index.ts
@@ -1,37 +1,38 @@
 #!/usr/bin/env -S deno run --allow-read --allow-write --allow-net --allow-env
 
-import {
-  Select,
-  type SelectOption,
-} from "https://deno.land/x/cliffy@v1.0.0-rc.3/prompt/select.ts";
+import { Select } from "https://deno.land/x/cliffy@v1.0.0-rc.3/prompt/select.ts";
 import { executeCreateIssuesBulk } from "../src/create-issues-bulk/index.ts";
 import { ensureEnvToken } from "../utils/env.ts";
 
-const menuOptions: SelectOption<string>[] = [
+const menuOptions = [
   {
     name: "Bulk Issue Creation",
     value: "create-issues-bulk",
+    execute: executeCreateIssuesBulk,
   },
   {
     name: "Exit",
     value: "exit",
+    execute: () => {
+      Deno.exit(0);
+    },
   },
 ];
 
 async function main(): Promise<void> {
   await ensureEnvToken();
 
-  const choice = await Select.prompt({
+  const choice = await Select.prompt<string>({
     message: "Please select:",
     options: menuOptions,
   });
 
-  switch (choice) {
-    case "create-issues-bulk":
-      await executeCreateIssuesBulk();
-      break;
-    case "exit":
-      Deno.exit(0);
+  const execute = menuOptions.find(
+    (option) => option.value === choice
+  )?.execute;
+
+  if (execute) {
+    await execute();
   }
 }
 
